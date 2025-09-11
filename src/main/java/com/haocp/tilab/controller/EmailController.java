283a40a4e293e5e952da3ef8db83365db925d6a5
haocp <haocpse184107@fpt.edu.vn>
2025-09-11 21:44:29 +0700

    Update EmailController.java

--- src/main/java/com/haocp/tilab/controller/EmailController.java
+++ src/main/java/com/haocp/tilab/controller/EmailController.java
@@ -1,7 +1,11 @@
 package com.haocp.tilab.controller;
 
+import com.haocp.tilab.dto.ApiResponse;
+import com.haocp.tilab.entity.EmailTemplate;
+import com.haocp.tilab.service.EmailTemplateService;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/emails/")
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class EmailController {
+
+    @Autowired
+    EmailTemplateService emailTemplateService;
+
+    public ApiResponse<EmailTemplate> createTemplate(){
+        return ApiResponse.<EmailTemplate>builder()
+                .build();
+    }
+
 }
