111ca1b99e867ef384a6bb29f419bbb430c87346
haocp <haocpse184107@fpt.edu.vn>
2025-09-26 00:17:04 +0700

    init rv and dashboard controller

--- src/main/java/com/haocp/tilab/controller/DashboardController.java
+++ src/main/java/com/haocp/tilab/controller/DashboardController.java
@@ -2,11 +2,13 @@ package com.haocp.tilab.controller;
 
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/dash-boards")
+@PreAuthorize("hasAuthority('ADMIN')")
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class DashboardController {
 }
--- src/main/java/com/haocp/tilab/controller/ReviewController.java
+++ src/main/java/com/haocp/tilab/controller/ReviewController.java
@@ -1,15 +1,33 @@
 package com.haocp.tilab.controller;
 
+import com.haocp.tilab.dto.ApiResponse;
+import com.haocp.tilab.dto.request.Review.CreateReviewRequest;
+import com.haocp.tilab.dto.response.Review.ReviewResponse;
+import com.haocp.tilab.service.ReviewService;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/reviews")
+@RequestMapping("/api")
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ReviewController {
 
+    @Autowired
+    ReviewService reviewService;
 
+    @PostMapping(value = "/bags/{bagId}/reviews", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ApiResponse<ReviewResponse> createReview(@RequestParam(value = "review") CreateReviewRequest request,
+                                                    @RequestParam(value = "reviewImage", required = false) List<MultipartFile> reviewImages, @PathVariable String bagId) {
+
+        return ApiResponse.<ReviewResponse>builder()
+                .data(reviewService.creatReview(bagId, request, reviewImages))
+                .build();
+    }
 
 }
